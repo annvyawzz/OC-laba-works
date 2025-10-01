55e119c0577f28058a57bf5a18756878a946dc79
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-10-01 18:48:13 +0300

    Update file.cpp

--- laba2/file.cpp
+++ laba2/file.cpp
@@ -2,3 +2,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+using namespace std;
+
+const char* CHILD_ARG = "child";
+void PrintError(const string& message)
+{
+    cerr << "Ошибка: " << message << " (код: " << GetLastError() << ")" << endl;
+
+  int count = 0;
+for (int i = 0; i < size; ++i) 
+{
+    if (array[i] % 3 == 0) {
+        count++;
+    }
+}
+}
+
