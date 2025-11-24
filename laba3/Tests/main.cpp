0de4983f9e6dd0afe6b13d8c619f002fb7f8c7f7
Anyta Derkach Fem <annaderkac8@gmail.com>
2025-11-24 21:31:49 +0300

    Update main.cpp

--- laba3/Tests/main.cpp
+++ laba3/Tests/main.cpp
@@ -1,35 +1,39 @@
 #include "pch.h"
 #include "MarkerManager.h"
 #include <iostream>
+#include <locale>
+using namespace std;
 
 int main(int argc, char** argv)
 {
-    // Всегда запускаем тесты, потом интерактивный режим
-    std::cout << "=== ЗАПУСК ТЕСТОВ ===" << std::endl;
+    setlocale(LC_ALL, "rus");
+
+    cout << "=== ЗАПУСК ТЕСТОВ ===" << endl;
     ::testing::InitGoogleTest(&argc, argv);
     int testResult = RUN_ALL_TESTS();
 
-    std::cout << "\n=== ИНТЕРАКТИВНЫЙ РЕЖИМ ===" << std::endl;
+   cout << "\n=== ИНТЕРАКТИВНЫЙ РЕЖИМ ===" << endl;
     MarkerManager manager;
     int arraySize, markerCount;
 
-    std::cout << "Введите размер массива: ";
-    std::cin >> arraySize;
-    std::cout << "Введите количество потоков marker: ";
-    std::cin >> markerCount;
+   cout << "Введите размер массива: ";
+    cin >> arraySize;
+   cout << "Введите количество потоков marker: ";
+   cin >> markerCount;
 
-    if (manager.Initialize(arraySize, markerCount)) {
+    if (manager.Initialize(arraySize, markerCount))
+    {
         manager.Run();
     }
     else {
-        std::cout << "Ошибка инициализации!" << std::endl;
+       cout << "Ошибка инициализации!" << endl;
     }
 
     manager.Cleanup();
 
-    std::cout << "Нажмите Enter для выхода...";
-    std::cin.ignore();
-    std::cin.get();
+   cout << "Нажмите Enter для выхода...";
+   cin.ignore();
+   cin.get();
 
     return testResult;
 }
